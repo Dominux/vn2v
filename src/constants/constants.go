0f3b83a013196d932e09dba8b196a4dcdddcab23
Dominux <kir.gimranov@gmail.com>
2025-07-06 15:15:52 +0300

    Added some code no one cares of except me

---
+++ src/constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	InputVN string = "input_vn"
+)
--- src/vn_extractor/vn_extractor.go
+++ src/vn_extractor/vn_extractor.go
@@ -1,14 +1,39 @@
 package vnextractor
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/Dominux/vn2vn/constants"
+	"github.com/google/uuid"
+)
+
 type VNExtractor struct {
-	basePath *string
+	basePath string
 }
 
-func NewVNExtractor(basePath *string) *VNExtractor {
-	return &VNExtractor{basePath: basePath}
+func NewVNExtractor(basePath string) (*VNExtractor, error) {
+	// Removing base path
+	removeBasePath(basePath)
+
+	// Making base path
+	err := os.MkdirAll(basePath, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	return &VNExtractor{basePath: basePath}, nil
 }
 
-func ExtractAudio(pathToVN *string) {
-	// fullPath :=
-	command := "ffmpeg -i "
+func (v *VNExtractor) ExtractAudio(id uuid.UUID) {
+	fullPath := v.buildFullPath(id, constants.InputVN)
+	command := fmt.Sprintf("ffmpeg -i %s")
+}
+
+func (v *VNExtractor) buildFullPath(id uuid.UUID, filename string) string {
+	return fmt.Sprintf("%s/%s/%s", v.basePath, id.String(), filename)
+}
+
+func removeBasePath(basePath string) error {
+	return os.RemoveAll(basePath)
 }
